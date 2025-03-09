62a39d7d5d0a8cff607d8eaa484da4ef50bf1acf
Aaron Gobeyn <aaron.gobeyn@gmail.com>
2025-03-09 12:16:34 +0100

    Replace simple weather shell script with more informative python script, it adds forecasts in the tooltip and includes soms additional information.

--- waybar/.config/waybar/scripts/weather.sh
+++ waybar/.config/waybar/scripts/weather.sh
@@ -4,4 +4,5 @@ LOCATION=darmstadt
 
 WEATHER=$(curl -s "https://wttr.in/$LOCATION?format=%c+%m+%t\n")
 
-printf '{"text": "%s", "alt": "%s", "tooltip": "%s", "class": "blank"}' "$WEATHER" "$WEATHER" "$WEATHER"
+# printf '{"text": "%s", "alt": "%s", "tooltip": "%s", "class": "blank"}' "$WEATHER" "$WEATHER" "$WEATHER"
+printf '{"text": "%s", "alt": "%s", "tooltip": "%s"}' "$WEATHER" "$WEATHER" "$WEATHER"
